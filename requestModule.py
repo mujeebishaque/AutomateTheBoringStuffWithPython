3797e5d1b88d60cf0544098e0e4a05bfedc390f3
mujeebishaque <nhawk1100@gmail.com>
2018-04-10 21:28:34 +0500

    workingWithWebModules

---
+++ requestModule.py
@@ -0,0 +1,28 @@
+import requests
+
+response = requests.get('url')
+
+result = response.status_code
+
+# suppose we got text from web_page, let's save it in b mode
+
+file = open('someFile.txt', 'wb')
+# wb => write binary
+
+for chunk in response.iter_content(10000):
+    # iter_content returns chunk of data in bytes.
+    # pass the num of bytes per iter in method above.
+    file.write(chunk)
+
+file.close()
+
+"""
+raise_for_status() raises an exception
+
+if result == 400 or result == 404:
+    pass
+else:
+    # save the file
+    pass
+better to use try and catch
+"""
---
+++ webbrowserModule.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+# for lauching and remotely controlling browsers
+
+webbrowser.open('url_here')
+webbrowser.get('static_url')
