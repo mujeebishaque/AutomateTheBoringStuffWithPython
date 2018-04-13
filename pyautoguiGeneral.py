599fa02ff360fef190c45a5c3e03f51f48248578
mujeebishaque <nhawk1100@gmail.com>
2018-04-14 02:44:11 +0500

    working with pyautogui

---
+++ pyautoguiGeneral.py
@@ -0,0 +1,15 @@
+
+'''
+
+lets you use awesome functions to 
+control keyboard, mouse and 
+take screenshots
+
+pyautogui.screenshot()
+pyautogui.size()->screen resolution
+
+pyautogui.locateOnScreen()->returns position of 
+object/image found, takes image-to-be-found as an
+argument. recognizes image on screen.. !!
+
+'''
---
+++ pyautogui_keyboard.py
@@ -0,0 +1,9 @@
+
+# import pyautogui
+
+# it will focus on active window
+
+
+# pyautogui.typewrite()-> string of chars to type
+# pyautogui.press()-> lets a single key press
+# pyautogui.hotkeys()-> keyboard shortcuts like Ctrl+O
