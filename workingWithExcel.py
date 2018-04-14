7217141b4085d9fdbe9daf8c03e824e3762124ea
mujeebishaque <nhawk1100@gmail.com>
2018-04-14 21:40:12 +0500

    openpyxl.py Working with Excel

---
+++ workingWithExcel.py
@@ -0,0 +1,23 @@
+import openpyxl
+
+workbook = openpyxl.load_workbook('cwd_file.xlxs')
+
+# a sinle xlxs file might contain multiple tabs and 
+# or maybe you can call that a sheet
+# that little tab thingy in the left-bottom of your 
+# excel file??? yeah, that's what it is... that's a sheet
+
+# you can select a sheet by
+
+sheet1 = workbook.get_sheet_by_name('sheet_name_i.e_sheet1')
+
+# get names of all sheets, no paras
+
+sheets = workbook.get_sheet_names()
+# print (sheets) will result in sheet1
+
+# let's suppose we get a sheet named sheet1, let's work with it
+cellOrColumnName = sheet1['firstColumnName']
+store = str(cellOrColumnName.value)
+ 
+#  cell() method retusn a Cell object from a sheet
