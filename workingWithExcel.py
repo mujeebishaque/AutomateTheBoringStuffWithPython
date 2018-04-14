d65f60087a742d7e03af0e5f483625eed1e55057
mujeebishaque <nhawk1100@gmail.com>
2018-04-14 22:22:40 +0500

    Working with Excel Completed

--- workingWithExcel.py
+++ workingWithExcel.py
@@ -23,5 +23,53 @@ cellOrColumnName = sheet1['firstColumnName']
 # with all the contents of that cell. 
 
 store = str(cellOrColumnName.value)
- 
 #  cell() method retusn a Cell object from a sheet
+
+
+# EDITING EXCEL SPREADSHEETS
+wb = openpyxl.workbook()
+# create a workbook object
+wb.get_sheet_names()
+# ['Sheet'] -> default name prolly. 
+# lets select this sheet and work with it
+
+sheet = wb.get_sheet_by_name('Sheet')
+
+sheet['A1'].value == None
+# returns true
+
+sheet['A1'] = 42
+sheet['A2'] = 'Hello'
+
+# save the sheet to some other directory. 
+
+import os
+
+os.chdir('c:\\Users\\UserName\\Documents')
+wb.save()
+
+# create another sheet
+sheet2 = wb.create_sheet()
+wb.get_sheet_names()
+
+# would returns 2 sheets now
+# [Sheet1, Sheet2]
+
+sheet2.title = 'New Sheet Of People Name'
+wb.get_sheet_names()
+
+# now it will return sheet1 and New Sheet of People Name
+wb.save('example.xlsx')
+# workbook saved
+
+wb.create_sheet(index=0, title='My Other Sheet')
+wb.save('Other Sheet.xlsx')
+# Now, this workbook would have first sheet named my other sheet
+# as you can see the first para index=0, represents first sheet index
+
+
+
+
+
+
+
