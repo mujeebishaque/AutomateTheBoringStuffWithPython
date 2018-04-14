eca039cf6c62d271c42db138e623199489d719bd
mujeebishaque <nhawk1100@gmail.com>
2018-04-14 23:54:03 +0500

    Working WIth PDF's

---
+++ WorkingWIthPDFs.py
@@ -0,0 +1,13 @@
+# Nothing complex
+# import PyPDF2
+# learn the case
+
+# open a pdf by calling open('path', 'rb')
+
+# to get page number getPage()
+# get text from that page extractText()
+
+# To generate a new PDF file use PdfFileWriter()
+# you can append new page using addPage() method
+
+# to save the changes, you need to save it using write() function.
